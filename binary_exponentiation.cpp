3bff1310b789d327a5402cf3546295d74dc9deca
Yana541 <149325426+Yana541@users.noreply.github.com>
2024-03-10 22:08:20 +0200

    Create main cpp file

---
+++ binary_exponentiation.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int bin(int a, int n)
+{
+	if (n == 0)
+		return 1;
+	if (n % 2 == 1)
+		return bin(a, n - 1) * a;
+	else
+	{
+		int b = bin(a, n / 2);
+		return b * b;
+	}
+}
+
+int main()
+{
+	int a, n;
+	cin >> a >> n;
+	cout << bin(a, n);
+	return 0;
+}
+
